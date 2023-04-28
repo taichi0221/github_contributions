9e59e2a7bf98b11f2e3e9e6ee01e1e2b356f07c6
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-04-28 11:16:48 +0900

    nokogiri

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,4 +1,21 @@
 class CalendarController < ApplicationController
+  include HTTParty
+
   def index
+    @contributions = fetch_github_contributions
   end
+
+  private
+
+  def fetch_github_contributions
+    # ここにあなたのGitHubユーザー名を入力してください
+    github_username = "taichi0221"
+    url = "https://github-contributions-api.now.sh/v1/#{github_username}"
+  
+    response = self.class.get(url)
+    parsed_response = JSON.parse(response.body)
+    puts parsed_response.inspect # デバッグ用の出力
+    return parsed_response["contributions"]
+  end
+  
 end
